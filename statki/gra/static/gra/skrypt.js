a75bc5271702f068eaa33bba8153ce5fec2173ff
lukaszrud <LukaszRud@DOM.SCI.EDU.PL>
2024-12-17 22:07:27 +0100

    Działa zaznaczanie pól i wysyła wybrane pola do serwera

---
+++ statki/gra/static/gra/skrypt.js
@@ -0,0 +1,20 @@
+const wybranePola = new Set();
+
+document.querySelectorAll('.pole').forEach(pole => {
+    pole.addEventListener('click', () => {
+        const poleID = pole.id;
+        if (wybranePola.has(poleID)) {
+            wybranePola.delete(poleID);
+            pole.classList.remove('zaznaczone');
+        } else {
+            wybranePola.add(poleID);
+            pole.classList.add('zaznaczone');
+        }
+    });
+});
+
+document.getElementById('potwierdz_plansze').addEventListener('click', () => {
+    const tab = Array.from(wybranePola);
+    document.getElementById('wybrane_pola').value = JSON.stringify(tab);
+    document.getElementById('form').submit();
+});
--- statki/gra/views.py
+++ statki/gra/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
+import json
 
 def lista(request):
     return render(request, "gra/lista.html")
 
 def nowa_gra(request):
+
+    if request.method == 'POST':
+        wybrane_pola = request.POST.get('wybrane_pola')
+        if wybrane_pola:
+            wybrane_pola = json.loads(wybrane_pola)     # zmiana z jsona na listę
+            print(f"Zaznaczone pola: {wybrane_pola}")
+    
     context = {
-        'liczby': range(10)     # lista liczb od 0 do 9 - potrzebna aby wygenerować tabelę 10x10 
+        'liczby': range(10)     # lista liczb od 0 do 9 - aby wygenerować tabelę 10x10 
     }
+
     return render(request, "gra/nowa.html", context)
